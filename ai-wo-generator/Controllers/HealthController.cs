8dc13eda1a759ae5a478751fa10a62bb61890722
Harjot <singh.harjot85@gmail.com>
2025-11-13 18:01:57 -0800

    Remove boilerplate | Add Health check | Add task list

---
+++ ai-wo-generator/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ai_wo_generator.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("health")]
+        public IActionResult GetHealthStatus()
+        {
+            return Ok(new { status = "Healthy" });
+        }
+    }
+}
